090f6beacf7476c77e5e13a2ab599b56bfa0c900
nakun1 <nakuni@nakuni.com>
2021-03-31 19:13:57 +0200

    Premiere version fonctionelle

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnUpdateRF2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOpenWorkshop = new System.Windows.Forms.Button();
+            this.btnDlMod = new System.Windows.Forms.Button();
+            this.txtIdMod = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnUpdateRF2
+            // 
+            this.btnUpdateRF2.Location = new System.Drawing.Point(68, 20);
+            this.btnUpdateRF2.Name = "btnUpdateRF2";
+            this.btnUpdateRF2.Size = new System.Drawing.Size(133, 23);
+            this.btnUpdateRF2.TabIndex = 0;
+            this.btnUpdateRF2.Text = "Mise à jour RF2";
+            this.btnUpdateRF2.UseVisualStyleBackColor = true;
+            this.btnUpdateRF2.Click += new System.EventHandler(this.btnUpdateRF2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(213, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Met à jour le serveur si nouvelle version d\'RF2";
+            // 
+            // btnOpenWorkshop
+            // 
+            this.btnOpenWorkshop.Location = new System.Drawing.Point(68, 50);
+            this.btnOpenWorkshop.Name = "btnOpenWorkshop";
+            this.btnOpenWorkshop.Size = new System.Drawing.Size(133, 23);
+            this.btnOpenWorkshop.TabIndex = 2;
+            this.btnOpenWorkshop.Text = "Ouvrir Steam Workshop";
+            this.btnOpenWorkshop.UseVisualStyleBackColor = true;
+            this.btnOpenWorkshop.Click += new System.EventHandler(this.btnOpenWorkshop_Click);
+            // 
+            // btnDlMod
+            // 
+            this.btnDlMod.Location = new System.Drawing.Point(252, 181);
+            this.btnDlMod.Name = "btnDlMod";
+            this.btnDlMod.Size = new System.Drawing.Size(133, 23);
+            this.btnDlMod.TabIndex = 3;
+            this.btnDlMod.Text = "Télécharger le Mod";
+            this.btnDlMod.UseVisualStyleBackColor = true;
+            this.btnDlMod.Click += new System.EventHandler(this.btnDlMod_Click);
+            // 
+            // txtIdMod
+            // 
+            this.txtIdMod.Location = new System.Drawing.Point(216, 155);
+            this.txtIdMod.Name = "txtIdMod";
+            this.txtIdMod.Size = new System.Drawing.Size(169, 20);
+            this.txtIdMod.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ID du mod à télécharger :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(445, 224);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIdMod);
+            this.Controls.Add(this.btnDlMod);
+            this.Controls.Add(this.btnOpenWorkshop);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUpdateRF2);
+            this.Name = "Form1";
+            this.Text = "Prouuuuuuuuuuuuuut";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnUpdateRF2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOpenWorkshop;
+        private System.Windows.Forms.Button btnDlMod;
+        private System.Windows.Forms.TextBox txtIdMod;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,18 +1,146 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
+using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace InstallPackageRF2
 {
     public partial class Form1 : Form
     {
+        public string PathRFactor = Environment.CurrentDirectory + @"\..\rfactor2-dedicated";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Executes a shell command synchronously.
+        /// </summary>
+        /// <param name="command">string command</param>
+        /// <remarks>Trouvé ici: https://stackoverflow.com/a/59235057</remarks>
+        /// <returns>string, as output of the command.</returns>
+        private static void ExecuteCommandSync(object command)
+        {
+            try
+            {
+                // create the ProcessStartInfo using "cmd" as the program to be run, and "/c " as the parameters.
+                // Incidentally, /c tells cmd that we want it to execute the command that follows, and then exit.
+                System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
+                // The following commands are needed to redirect the standard output. 
+                //This means that it will be redirected to the Process.StandardOutput StreamReader.
+                procStartInfo.RedirectStandardOutput = false;
+                procStartInfo.UseShellExecute = false;
+                // Do not create the black window.
+                procStartInfo.CreateNoWindow = false;
+                // Now we create a process, assign its ProcessStartInfo and start it
+                System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                proc.StartInfo = procStartInfo;
+                proc.Start();
+                proc.WaitForExit();
+
+                //// Get the output into a string
+                //string result = proc.StandardOutput.ReadToEnd();
+
+                //// Display the command output.
+                //Console.WriteLine(result);
+            }
+            catch (Exception objException)
+            {
+                // Log the exception
+                MessageBox.Show("ExecuteCommandSync failed" + objException.Message);
+            }
+        }
+
+        private void btnUpdateRF2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = '"' + Environment.CurrentDirectory + @"\steamcmd.exe" + '"';
+                string cmd = path + @" +login anonymous +force_install_dir ../rFactor2-Dedicated +app_update 400300 +quit";
+                this.txtDebug.Text = cmd;
+                ExecuteCommandSync(cmd);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur : " + ex.Message);
+            }
+        }
+
+        private void btnOpenWorkshop_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://steamcommunity.com/app/365960/workshop/");
+        }
+
+        private void btnDlMod_Click(object sender, EventArgs e)
+        {
+            long idMod;
+
+            try
+            {
+                if (string.IsNullOrEmpty(this.txtIdMod.Text.Trim()))
+                {
+                    MessageBox.Show("Il faut mettre un ID boubourse!");
+                }
+                else if (!long.TryParse(this.txtIdMod.Text.Trim(), out idMod))
+                {
+                    MessageBox.Show("L'ID doit être un nombre ptain!");
+                }
+                else
+                {
+                    string pathSteamcmd = '"' + Environment.CurrentDirectory + @"\steamcmd.exe" + '"';
+                    string cmd = pathSteamcmd + " +login anonymous +workshop_download_item 365960 " + idMod + " +quit";
+                    ExecuteCommandSync(cmd);
+
+                    this.txtDebug.Text = cmd;
+
+                    string pathMod =  Environment.CurrentDirectory + @"\steamapps\workshop\content\365960\" + idMod ;
+                    string pathPackage = this.PathRFactor + @"\Packages\";
+                    if (!Directory.Exists(pathMod))
+                    {
+                        throw new Exception("Le mod n'a pas été telechargé, vérifier l'ID.");
+                    }
+
+                    // On déplace les fichiers téléchargés dans le dossier Packages de RF2
+                    List<string> files = Directory.GetFiles(pathMod).ToList();
+                    FileInfo fileInfo;
+
+                    foreach (string file in files)
+                    {
+                        fileInfo = new FileInfo(file);
+
+                        string pathPackageFull =  pathPackage + @"\" + fileInfo.Name ;
+
+                        if (File.Exists(pathPackageFull))
+                        {
+                            File.Delete(pathPackageFull);
+                        }
+
+                        File.Move(file, pathPackageFull);
+
+                        // Installer le mod avec modmgr, ne fonctionne pas :(
+                        //try
+                        //{
+                        //    cmd = this.PathRFactor + @"\Bin64\ModMgr.exe -i" + fileInfo.Name
+                        //        + @" -p" + pathPackage + @" -d" + this.PathRFactor;
+                        //    ExecuteCommandSync(cmd);
+                        //}
+                        //catch
+                        //{
+                        //    // On ne fait rien pour ne pas bloquer le reste du téléchargement, au pire on installera le mod manuellement
+                        //}
+                    }
+
+                    Directory.Delete(pathMod);
+
+                    MessageBox.Show("Ayé");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erreur : " + ex.Message);
+            }
         }
     }
 }
