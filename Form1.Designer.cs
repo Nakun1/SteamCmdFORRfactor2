7d59fd3817f95730d94d48215bee4758173e5493
Fred <fred@exemple.com>
2026-07-27 15:49:20 +0200

    ajout AMS2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace InstallPackageRF2
+namespace InstallPackageRF2
 {
     partial class Form1
     {
@@ -10,7 +10,7 @@
         /// <summary>
         /// Nettoyage des ressources utilisées.
         /// </summary>
-        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -28,82 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnUpdateRF2 = new System.Windows.Forms.Button();
+            this.groupBoxGame = new System.Windows.Forms.GroupBox();
+            this.rbAMS2 = new System.Windows.Forms.RadioButton();
+            this.rbRFactor2 = new System.Windows.Forms.RadioButton();
+            this.btnUpdateServer = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.btnOpenWorkshop = new System.Windows.Forms.Button();
             this.btnDlMod = new System.Windows.Forms.Button();
             this.txtIdMod = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.groupBoxGame.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // btnUpdateRF2
-            // 
-            this.btnUpdateRF2.Location = new System.Drawing.Point(68, 20);
-            this.btnUpdateRF2.Name = "btnUpdateRF2";
-            this.btnUpdateRF2.Size = new System.Drawing.Size(133, 23);
-            this.btnUpdateRF2.TabIndex = 0;
-            this.btnUpdateRF2.Text = "Mise à jour RF2";
-            this.btnUpdateRF2.UseVisualStyleBackColor = true;
-            this.btnUpdateRF2.Click += new System.EventHandler(this.btnUpdateRF2_Click);
-            // 
+            //
+            // groupBoxGame
+            //
+            this.groupBoxGame.Controls.Add(this.rbAMS2);
+            this.groupBoxGame.Controls.Add(this.rbRFactor2);
+            this.groupBoxGame.Location = new System.Drawing.Point(12, 9);
+            this.groupBoxGame.Name = "groupBoxGame";
+            this.groupBoxGame.Size = new System.Drawing.Size(421, 45);
+            this.groupBoxGame.TabIndex = 0;
+            this.groupBoxGame.TabStop = false;
+            this.groupBoxGame.Text = "Serveur";
+            //
+            // rbAMS2
+            //
+            this.rbAMS2.AutoSize = true;
+            this.rbAMS2.Location = new System.Drawing.Point(150, 18);
+            this.rbAMS2.Name = "rbAMS2";
+            this.rbAMS2.Size = new System.Drawing.Size(115, 17);
+            this.rbAMS2.TabIndex = 1;
+            this.rbAMS2.Text = "Automobilista 2";
+            this.rbAMS2.UseVisualStyleBackColor = true;
+            this.rbAMS2.CheckedChanged += new System.EventHandler(this.rbGame_CheckedChanged);
+            //
+            // rbRFactor2
+            //
+            this.rbRFactor2.AutoSize = true;
+            this.rbRFactor2.Checked = true;
+            this.rbRFactor2.Location = new System.Drawing.Point(15, 18);
+            this.rbRFactor2.Name = "rbRFactor2";
+            this.rbRFactor2.Size = new System.Drawing.Size(70, 17);
+            this.rbRFactor2.TabIndex = 0;
+            this.rbRFactor2.TabStop = true;
+            this.rbRFactor2.Text = "rFactor 2";
+            this.rbRFactor2.UseVisualStyleBackColor = true;
+            this.rbRFactor2.CheckedChanged += new System.EventHandler(this.rbGame_CheckedChanged);
+            //
+            // btnUpdateServer
+            //
+            this.btnUpdateServer.Location = new System.Drawing.Point(68, 70);
+            this.btnUpdateServer.Name = "btnUpdateServer";
+            this.btnUpdateServer.Size = new System.Drawing.Size(133, 23);
+            this.btnUpdateServer.TabIndex = 2;
+            this.btnUpdateServer.Text = "Mise à jour";
+            this.btnUpdateServer.UseVisualStyleBackColor = true;
+            this.btnUpdateServer.Click += new System.EventHandler(this.btnUpdateServer_Click);
+            //
             // label1
-            // 
+            //
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(213, 25);
+            this.label1.Location = new System.Drawing.Point(213, 75);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(224, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Met à jour le serveur si nouvelle version d\'RF2";
-            // 
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Met à jour le serveur si nouvelle version";
+            //
             // btnOpenWorkshop
-            // 
-            this.btnOpenWorkshop.Location = new System.Drawing.Point(68, 50);
+            //
+            this.btnOpenWorkshop.Location = new System.Drawing.Point(68, 100);
             this.btnOpenWorkshop.Name = "btnOpenWorkshop";
             this.btnOpenWorkshop.Size = new System.Drawing.Size(133, 23);
-            this.btnOpenWorkshop.TabIndex = 2;
+            this.btnOpenWorkshop.TabIndex = 4;
             this.btnOpenWorkshop.Text = "Ouvrir Steam Workshop";
             this.btnOpenWorkshop.UseVisualStyleBackColor = true;
             this.btnOpenWorkshop.Click += new System.EventHandler(this.btnOpenWorkshop_Click);
-            // 
+            //
             // btnDlMod
-            // 
-            this.btnDlMod.Location = new System.Drawing.Point(252, 181);
+            //
+            this.btnDlMod.Location = new System.Drawing.Point(252, 231);
             this.btnDlMod.Name = "btnDlMod";
             this.btnDlMod.Size = new System.Drawing.Size(133, 23);
-            this.btnDlMod.TabIndex = 3;
+            this.btnDlMod.TabIndex = 5;
             this.btnDlMod.Text = "Télécharger le Mod";
             this.btnDlMod.UseVisualStyleBackColor = true;
             this.btnDlMod.Click += new System.EventHandler(this.btnDlMod_Click);
-            // 
+            //
             // txtIdMod
-            // 
-            this.txtIdMod.Location = new System.Drawing.Point(216, 155);
+            //
+            this.txtIdMod.Location = new System.Drawing.Point(216, 205);
             this.txtIdMod.Name = "txtIdMod";
             this.txtIdMod.Size = new System.Drawing.Size(169, 20);
-            this.txtIdMod.TabIndex = 4;
-            // 
+            this.txtIdMod.TabIndex = 6;
+            //
             // label2
-            // 
+            //
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(74, 158);
+            this.label2.Location = new System.Drawing.Point(74, 208);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(127, 13);
-            this.label2.TabIndex = 5;
+            this.label2.TabIndex = 7;
             this.label2.Text = "ID du mod à télécharger :";
-            // 
+            //
             // Form1
-            // 
+            //
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(445, 224);
+            this.ClientSize = new System.Drawing.Size(445, 274);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtIdMod);
             this.Controls.Add(this.btnDlMod);
             this.Controls.Add(this.btnOpenWorkshop);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnUpdateRF2);
+            this.Controls.Add(this.btnUpdateServer);
+            this.Controls.Add(this.groupBoxGame);
             this.Name = "Form1";
             this.Text = "Prouuuuuuuuuuuuuut";
+            this.groupBoxGame.ResumeLayout(false);
+            this.groupBoxGame.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,7 +153,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnUpdateRF2;
+        private System.Windows.Forms.GroupBox groupBoxGame;
+        private System.Windows.Forms.RadioButton rbAMS2;
+        private System.Windows.Forms.RadioButton rbRFactor2;
+        private System.Windows.Forms.Button btnUpdateServer;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnOpenWorkshop;
         private System.Windows.Forms.Button btnDlMod;
@@ -119,4 +164,3 @@
         private System.Windows.Forms.Label label2;
     }
 }
-
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -8,30 +8,149 @@ namespace InstallPackageRF2
 {
     public partial class Form1 : Form
     {
-        public string PathRFactor = Environment.CurrentDirectory + @"\..\rfactor2-dedicated";
+        private class GameConfig
+        {
+            public string DisplayName;
+            public string SteamAppId;
+            public string InstallDirRelative;
+            public bool HasWorkshop;
+            public string WorkshopAppId;
+        }
+
+        private static readonly GameConfig RFactor2Config = new GameConfig
+        {
+            DisplayName = "rFactor 2",
+            SteamAppId = "400300",
+            InstallDirRelative = "../rFactor2-Dedicated",
+            HasWorkshop = true,
+            WorkshopAppId = "365960"
+        };
+
+        private static readonly GameConfig AMS2Config = new GameConfig
+        {
+            DisplayName = "Automobilista 2",
+            SteamAppId = "1338040",
+            InstallDirRelative = "../AMS2-Dedicated",
+            HasWorkshop = false,
+            WorkshopAppId = null
+        };
+
+        private GameConfig CurrentGame
+        {
+            get { return this.rbAMS2.Checked ? AMS2Config : RFactor2Config; }
+        }
+
+        private string PathServer
+        {
+            get { return Environment.CurrentDirectory + @"\" + this.CurrentGame.InstallDirRelative; }
+        }
+
+        private const string SteamCmdDownloadUrl = "https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip";
 
         public Form1()
         {
             InitializeComponent();
+            this.Load += new EventHandler(this.Form1_Load);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.SetControlsEnabled(false);
+            this.EnsureSteamCmdUpToDate();
+            this.SetControlsEnabled(true);
+            this.UpdateUiForSelectedGame();
+        }
+
+        private void SetControlsEnabled(bool enabled)
+        {
+            this.btnUpdateServer.Enabled = enabled;
+            this.btnOpenWorkshop.Enabled = enabled;
+            this.btnDlMod.Enabled = enabled;
+            this.txtIdMod.Enabled = enabled;
+            this.rbRFactor2.Enabled = enabled;
+            this.rbAMS2.Enabled = enabled;
+        }
+
+        /// <summary>
+        /// Télécharge et installe steamcmd.exe s'il est absent, puis force sa mise à jour
+        /// (steamcmd se met à jour tout seul dès qu'on l'exécute).
+        /// </summary>
+        private void EnsureSteamCmdUpToDate()
+        {
+            string originalTitle = this.Text;
+            string steamCmdPath = Environment.CurrentDirectory + @"\steamcmd.exe";
+
+            if (!File.Exists(steamCmdPath))
+            {
+                this.Text = originalTitle + " - Téléchargement de SteamCMD...";
+                this.Cursor = Cursors.WaitCursor;
+
+                string zipPath = Environment.CurrentDirectory + @"\steamcmd.zip";
+                try
+                {
+                    using (System.Net.WebClient client = new System.Net.WebClient())
+                    {
+                        client.DownloadFile(SteamCmdDownloadUrl, zipPath);
+                    }
+
+                    // tar.exe est intégré à Windows depuis la mise à jour 1803 et sait extraire les .zip.
+                    System.Diagnostics.ProcessStartInfo extractInfo = new System.Diagnostics.ProcessStartInfo(
+                        "tar", "-xf \"" + zipPath + "\" -C \"" + Environment.CurrentDirectory + "\"");
+                    extractInfo.UseShellExecute = false;
+                    extractInfo.CreateNoWindow = true;
+                    using (System.Diagnostics.Process proc = System.Diagnostics.Process.Start(extractInfo))
+                    {
+                        proc.WaitForExit();
+                    }
+
+                    if (!File.Exists(steamCmdPath))
+                    {
+                        MessageBox.Show("L'extraction de SteamCMD a échoué (tar.exe introuvable ou zip invalide).");
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Impossible de télécharger/installer SteamCMD automatiquement : " + ex.Message);
+                    return;
+                }
+                finally
+                {
+                    if (File.Exists(zipPath))
+                    {
+                        File.Delete(zipPath);
+                    }
+                    this.Cursor = Cursors.Default;
+                    this.Text = originalTitle;
+                }
+            }
+
+            this.Text = originalTitle + " - Mise à jour de SteamCMD...";
+            this.Cursor = Cursors.WaitCursor;
+            try
+            {
+                ExecuteCommandSync('"' + steamCmdPath + '"' + " +quit");
+            }
+            finally
+            {
+                this.Cursor = Cursors.Default;
+                this.Text = originalTitle;
+            }
         }
 
         /// <summary>
         /// Executes a shell command synchronously.
         /// </summary>
         /// <param name="command">string command</param>
-        /// <param name="keepWindowOpen">si vrai, la fenêtre cmd reste ouverte après l'exécution (utilise /k au lieu de /c)</param>
         /// <remarks>Trouvé ici: https://stackoverflow.com/a/59235057</remarks>
-        /// <returns>string, as output of the command.</returns>
-        private static void ExecuteCommandSync(object command, bool keepWindowOpen = false)
+        private static void ExecuteCommandSync(object command)
         {
             try
             {
                 // create the ProcessStartInfo using "cmd" as the program to be run, and "/c " as the parameters.
                 // Incidentally, /c tells cmd that we want it to execute the command that follows, and then exit.
-                // /k garde la fenêtre ouverte pour laisser la sortie visible.
-                string switchArg = keepWindowOpen ? "/k " : "/c ";
-                System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", switchArg + command);
-                // The following commands are needed to redirect the standard output. 
+                System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
+                // The following commands are needed to redirect the standard output.
                 //This means that it will be redirected to the Process.StandardOutput StreamReader.
                 procStartInfo.RedirectStandardOutput = false;
                 procStartInfo.UseShellExecute = false;
@@ -41,18 +160,7 @@ namespace InstallPackageRF2
                 System.Diagnostics.Process proc = new System.Diagnostics.Process();
                 proc.StartInfo = procStartInfo;
                 proc.Start();
-
-                // Si la fenêtre doit rester ouverte (/k), on ne bloque pas l'UI en attendant sa fermeture.
-                if (!keepWindowOpen)
-                {
-                    proc.WaitForExit();
-                }
-
-                //// Get the output into a string
-                //string result = proc.StandardOutput.ReadToEnd();
-
-                //// Display the command output.
-                //Console.WriteLine(result);
+                proc.WaitForExit();
             }
             catch (Exception objException)
             {
@@ -61,14 +169,102 @@ namespace InstallPackageRF2
             }
         }
 
-        private void btnUpdateRF2_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Exécute une commande sans fenêtre visible, en capturant toute sa sortie (stdout + stderr).
+        /// </summary>
+        /// <returns>true si une erreur a été détectée (code de sortie non nul ou "ERROR" dans la sortie).</returns>
+        private static bool ExecuteCommandCaptureOutput(string command, out string output)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+            System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + command);
+            procStartInfo.RedirectStandardOutput = true;
+            procStartInfo.RedirectStandardError = true;
+            procStartInfo.UseShellExecute = false;
+            procStartInfo.CreateNoWindow = true;
+
+            using (System.Diagnostics.Process proc = new System.Diagnostics.Process())
+            {
+                proc.StartInfo = procStartInfo;
+                proc.OutputDataReceived += (s, e) => { if (e.Data != null) sb.AppendLine(e.Data); };
+                proc.ErrorDataReceived += (s, e) => { if (e.Data != null) sb.AppendLine(e.Data); };
+
+                proc.Start();
+                proc.BeginOutputReadLine();
+                proc.BeginErrorReadLine();
+                proc.WaitForExit();
+
+                output = sb.ToString();
+                return proc.ExitCode != 0 || output.IndexOf("ERROR", StringComparison.OrdinalIgnoreCase) >= 0;
+            }
+        }
+
+        /// <summary>
+        /// Affiche une sortie de commande dans une fenêtre dédiée avec défilement.
+        /// </summary>
+        private static void ShowOutputDialog(string title, string output)
+        {
+            using (Form dialog = new Form())
+            {
+                dialog.Text = title;
+                dialog.Size = new System.Drawing.Size(700, 450);
+                dialog.StartPosition = FormStartPosition.CenterParent;
+
+                TextBox txtOutput = new TextBox();
+                txtOutput.Multiline = true;
+                txtOutput.ReadOnly = true;
+                txtOutput.ScrollBars = ScrollBars.Vertical;
+                txtOutput.WordWrap = false;
+                txtOutput.Dock = DockStyle.Fill;
+                txtOutput.Font = new System.Drawing.Font("Consolas", 9F);
+                txtOutput.Text = output;
+
+                dialog.Controls.Add(txtOutput);
+                dialog.ShowDialog();
+            }
+        }
+
+        private void rbGame_CheckedChanged(object sender, EventArgs e)
+        {
+            this.UpdateUiForSelectedGame();
+        }
+
+        private void UpdateUiForSelectedGame()
+        {
+            GameConfig game = this.CurrentGame;
+            this.label1.Text = "Met à jour le serveur si nouvelle version d'" + game.DisplayName;
+
+            // Pas de Steam Workshop pour ce jeu : on désactive les fonctionnalités liées.
+            this.btnOpenWorkshop.Enabled = game.HasWorkshop;
+            this.btnDlMod.Enabled = game.HasWorkshop;
+            this.txtIdMod.Enabled = game.HasWorkshop;
+            this.label2.Enabled = game.HasWorkshop;
+        }
+
+        private void btnUpdateServer_Click(object sender, EventArgs e)
         {
             try
             {
+                GameConfig game = this.CurrentGame;
                 string path = '"' + Environment.CurrentDirectory + @"\steamcmd.exe" + '"';
-                string cmd = path + @" +force_install_dir ../rFactor2-Dedicated +login anonymous +app_update 400300 +quit";
+                string cmd = path + @" +force_install_dir " + game.InstallDirRelative
+                    + " +login anonymous +app_update " + game.SteamAppId + " +quit";
                 //this.txtDebug.Text = cmd;
-                ExecuteCommandSync(cmd, keepWindowOpen: true);
+
+                string output;
+                bool hasError = ExecuteCommandCaptureOutput(cmd, out output);
+                if (hasError)
+                {
+                    ShowOutputDialog("Erreur lors de la mise à jour de " + game.DisplayName, output);
+                }
+                else if (output.IndexOf("already up to date", StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                    MessageBox.Show("Pas de mise à jour à faire, " + game.DisplayName + " est déjà à jour.");
+                }
+                else
+                {
+                    MessageBox.Show("Mise à jour de " + game.DisplayName + " réussie.");
+                }
             }
             catch (Exception ex)
             {
@@ -78,16 +274,28 @@ namespace InstallPackageRF2
 
         private void btnOpenWorkshop_Click(object sender, EventArgs e)
         {
-            System.Diagnostics.Process.Start("https://steamcommunity.com/app/365960/workshop/");
+            GameConfig game = this.CurrentGame;
+            if (!game.HasWorkshop)
+            {
+                MessageBox.Show(game.DisplayName + " n'a pas de Steam Workshop.");
+                return;
+            }
+
+            System.Diagnostics.Process.Start("https://steamcommunity.com/app/" + game.WorkshopAppId + "/workshop/");
         }
 
         private void btnDlMod_Click(object sender, EventArgs e)
         {
             long idMod;
+            GameConfig game = this.CurrentGame;
 
             try
             {
-                if (string.IsNullOrEmpty(this.txtIdMod.Text.Trim()))
+                if (!game.HasWorkshop)
+                {
+                    MessageBox.Show(game.DisplayName + " n'a pas de Steam Workshop.");
+                }
+                else if (string.IsNullOrEmpty(this.txtIdMod.Text.Trim()))
                 {
                     MessageBox.Show("Il faut mettre un ID boubourse!");
                 }
@@ -98,19 +306,19 @@ namespace InstallPackageRF2
                 else
                 {
                     string pathSteamcmd = '"' + Environment.CurrentDirectory + @"\steamcmd.exe" + '"';
-                    string cmd = pathSteamcmd + " +login anonymous +workshop_download_item 365960 " + idMod + " +quit";
+                    string cmd = pathSteamcmd + " +login anonymous +workshop_download_item " + game.WorkshopAppId + " " + idMod + " +quit";
                     ExecuteCommandSync(cmd);
 
                     //this.txtDebug.Text = cmd;
 
-                    string pathMod =  Environment.CurrentDirectory + @"\steamapps\workshop\content\365960\" + idMod ;
-                    string pathPackage = this.PathRFactor + @"\Packages\";
+                    string pathMod =  Environment.CurrentDirectory + @"\steamapps\workshop\content\" + game.WorkshopAppId + @"\" + idMod ;
+                    string pathPackage = this.PathServer + @"\Packages\";
                     if (!Directory.Exists(pathMod))
                     {
                         throw new Exception("Le mod n'a pas été telechargé, vérifier l'ID.");
                     }
 
-                    // On déplace les fichiers téléchargés dans le dossier Packages de RF2
+                    // On déplace les fichiers téléchargés dans le dossier Packages du serveur
                     List<string> files = Directory.GetFiles(pathMod).ToList();
                     FileInfo fileInfo;
 
@@ -130,8 +338,8 @@ namespace InstallPackageRF2
                         // Installer le mod avec modmgr, ne fonctionne pas :(
                         //try
                         //{
-                        //    cmd = this.PathRFactor + @"\Bin64\ModMgr.exe -i" + fileInfo.Name
-                        //        + @" -p" + pathPackage + @" -d" + this.PathRFactor;
+                        //    cmd = this.PathServer + @"\Bin64\ModMgr.exe -i" + fileInfo.Name
+                        //        + @" -p" + pathPackage + @" -d" + this.PathServer;
                         //    ExecuteCommandSync(cmd);
                         //}
                         //catch
